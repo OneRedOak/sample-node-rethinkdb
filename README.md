77e10ff04331bf7e913554c33d5b5311c506c474
Patrick <oneredoak@gmail.com>
2014-06-11 15:32:51 -0700

    Conversion to RethinkDB

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-Node.js CouchDB Sample [![Build Status](https://apibeta.shippable.com/projects/5397bdfeaa695d3e0076ff0d/badge/master)](https://beta.shippable.com/projects/5397bdfeaa695d3e0076ff0d)
+Node.js RethinkDB Sample [![Build Status](https://apibeta.shippable.com/projects/5398a9962f1c845800c968dc/badge/master)](https://beta.shippable.com/projects/5398a9962f1c845800c968dc)
 =================
 
-Implements a simple web application using CouchDB, with tests.
+Implements a simple web application using RethinkDB, with tests.
 
 Uses Grunt to run tests against an Express server, then generates reports with Xunit and Istanbul.
--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 var express = require("express"),
-    nano = require('nano')('http://localhost:5984'),
+    r = require('rethinkdb'),
     app = express();
 
-nano.db.destroy('test');
-nano.db.create('test');
-
-var db = nano.db.use('test');
+var connection = null;
+r.connect({ host: 'localhost', port: 28015 }, function(err, conn) {
+  if (err) {
+    conosle.log('Error connecting to RethinkDB: ' + err);
+  } else {
+    connection = conn;
+  }
+});
 
 app.get("/", function (req, res) {
   res.send("Hey buddy!");
 });
 
 app.get("/:name", function (req, res) {
+  r.table('table').filter(r.row('name').eq(req.params.name)).
+    run(connection, function(err, cursor) {
+      if(cursor.length < 1) {
+        r.table('table').insert({
+          name: req.params.name
+        }).run(connection, function(err, result) {
+          if (err) {
+            res.send(500);
+          }
+          res.send('Created a new thing with name ' + req.params.name);
+        });
+      } else {
+        cursor.toArray(function(err, result) {
+          if (err) {
+            console.log('Error converting data: ' + err);
+          }
+          console.log(JSON.stringify(result, null, 2));
+          res.send(JSON.stringify(result, null, 2));
+        });
+      }
+    });
+/*
   db.get(req.params.name, function (err, body) {
     if (body === undefined) {
       db.insert({'name': req.params.name}, req.params.name, function(err, b) {
@@ -25,6 +51,7 @@ app.get("/:name", function (req, res) {
       res.send(body);
     }
   });
+*/
 });
 
 app.listen(3000, function () {
